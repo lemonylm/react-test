66a51d111904f80d808fb6cbd22c41503428156d
mistletoe <549750956@qq.com>
2021-03-04 22:11:17 +0800

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Header from './components/Header'
+import Header from "./components/Header";
 function App() {
   return (
     <div className="todo-container">
-      <Header/>
+      <div className="todo-wrap">
+        <Header />
+      </div>
     </div>
   );
 }
