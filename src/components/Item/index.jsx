8d04b8e6477188d8f297e79ddfc605b2e285a721
mistletoe <549750956@qq.com>
2021-03-07 10:01:40 +0800

    hover

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 import "./index.css";
 export default class Item extends Component {
+  state = {
+    mouse: 'white',
+  };
+  handleMouse = (flag) => {
+    return () => {
+      if (flag) {
+        this.setState({ mouse: 'pink' });
+      }else{
+        this.setState({ mouse: 'white' });
+
+      }
+    };
+  };
   render() {
-    const {name,done} =this.props
+    const { name, done } = this.props;
     return (
-      <li>
+      <li
+        style={{ backgroundColor: this.state.mouse}}
+        onMouseEnter={this.handleMouse(true)}
+        onMouseLeave={this.handleMouse(false)}
+      >
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" defaultChecked={done} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: "none" }}>
-          删除
-        </button>
+        <button className="btn btn-danger">删除</button>
       </li>
     );
   }
